fc835ed121a7e1ce6f8babaa8edd49ba1ef622ef
PIRUMAL <pirumohanmalmaasitala@gmail.com>
2021-07-04 09:42:49 +0530

    test

---
+++ src/WrapperClass.java
@@ -0,0 +1,14 @@
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 int k=100;
+		 Integer i=new Integer(k);
+		 System.out.println(k*k);
+		 
+		 
+	}
+
+}
